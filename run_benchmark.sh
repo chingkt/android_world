96c68e2c74b5f3c3243c00acedaa19616eef69cb
Kei <kei.tang@sap.com>
2025-07-06 21:57:30 +0200

    run_benchmark with bluetooth

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,12 +1,12 @@
-TASK_NAME=SimpleSmsSendReceivedAddress
+TASK_NAME=SystemBluetoothTurnOnVerify
 
 for i in {1..1}; do
 python -u run.py \
   --suite_family=android_world \
   --agent_name=t3a_gemini_gcp \
-  --n_task_combinations=10 \
+  --n_task_combinations=3 \
   --tasks=$TASK_NAME \
-| tee -a ${TASK_NAME}_after_refinement.txt
+| tee -a ${TASK_NAME}.txt
   sleep 5
   done
 
