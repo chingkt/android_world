645fceb746ccd8237b05863c927f1a5b937c4679
chingkt <tangchingkei@gmail.com>
2025-08-06 17:04:44 +0200

    clean temp logs

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,12 +1,6 @@
-TASK_NAME=SystemBluetoothTurnOnVerify
-
-for i in {1..1}; do
 python -u run.py \
   --suite_family=android_world \
   --agent_name=t3a_gemini_gcp \
-  --n_task_combinations=3 \
-  --tasks=$TASK_NAME \
-| tee -a ${TASK_NAME}.txt
-  sleep 5
-  done
+  --n_task_combinations=1 \
+| tee -a benchmark_without_action_exec_prompt_and_memory_1time_3rd.txt
 
